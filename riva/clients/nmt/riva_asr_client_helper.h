0f31b1d5ca28deac037f609c85d1b1bf2d532b3f
Scot Junkin <sjunkin@nvidia.com>
2023-01-08 11:57:03 -0800

    feat: WIP speech translate (text|speech) clients

---
+++ riva/clients/nmt/riva_asr_client_helper.h
@@ -0,0 +1,31 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2022 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include <alsa/asoundlib.h>
+#include <grpcpp/grpcpp.h>
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#include "absl/strings/str_replace.h"
+
+std::vector<std::string> ReadBoostedPhrases(const std::string& boosted_phrases_file);
+
+bool WaitUntilReady(
+    std::shared_ptr<grpc::Channel> channel, std::chrono::system_clock::time_point& deadline);
+
+bool OpenAudioDevice(
+    const char* devicename, snd_pcm_t** handle, snd_pcm_stream_t stream_type, int channels,
+    int rate, unsigned int latency);
+
+bool CloseAudioDevice(snd_pcm_t** handle);
+
+std::string static inline EscapeTranscript(const std::string& input_str)
+{
+  return absl::StrReplaceAll(input_str, {{"\"", "\\\""}});
+}
