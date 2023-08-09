e4b0a8128d711347c487a04a515e601668d34b1a
Rahul Mittal <rmittal@nvidia.com>
2023-08-09 19:32:27 +0530

    Update usage string

--- riva/clients/nmt/riva_nmt_t2t_client.cc
+++ riva/clients/nmt/riva_nmt_t2t_client.cc
@@ -109,7 +109,7 @@ main(int argc, char** argv)
   FLAGS_logtostderr = 1;
 
   std::stringstream str_usage;
-  str_usage << "Usage: riva_nmt" << std::endl;
+  str_usage << "Usage: riva_nmt_t2t_client" << std::endl;
   str_usage << "           --text_file=<filename> " << std::endl;
   str_usage << "           --riva_uri=<server_name:port> " << std::endl;
   str_usage << "           --num_iterations=<integer> " << std::endl;
