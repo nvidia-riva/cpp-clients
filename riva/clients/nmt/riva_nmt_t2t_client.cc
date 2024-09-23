225134bea6701caa430b430669606d35b87e39c7
Manisha Johnson <manishaj@nvidia.com>
2024-09-23 13:45:26 +0530

    updated t2t client

--- riva/clients/nmt/riva_nmt_t2t_client.cc
+++ riva/clients/nmt/riva_nmt_t2t_client.cc
@@ -90,7 +90,7 @@ translateBatch(
   }
 }
 
-int countWords(const std::string& text) 
+int countWords(const std::string& text) {
     int wordCount = 0;
     bool wasSpace = true; 
     for (char c : text) {
