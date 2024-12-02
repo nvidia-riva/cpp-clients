1e1c1b64c777d107e57aa2dd60cb90a9a97e3577
Manisha Johnson <manishaj@nvidia.com>
2024-12-02 15:24:47 +0530

    resolving conflicts

--- riva/clients/nmt/riva_nmt_t2t_client.cc
+++ riva/clients/nmt/riva_nmt_t2t_client.cc
@@ -357,9 +357,10 @@ main(int argc, char** argv)
     std::chrono::duration<double> total = end - start;
     LOG(INFO) << FLAGS_model_name << "-" << FLAGS_batch_size << "-" << FLAGS_source_language_code
               << "-" << FLAGS_target_language_code << ",lines: " << count
-              << ",tokens: " << total_words << ",total time: " << total.count()
+              << ",tokens: " << total_words 
+              << ",total time: " << total.count()
               << ",requests/second: " << FLAGS_num_iterations * request_count / total.count()
-              << ",tokens/second: " << FLAGS_num_iterations * total_words / total.count();
+              << ",tokens/second: " << FLAGS_num_iterations * total_words /total.count();
 
     std::sort(latencies.begin(), latencies.end());
     auto size = latencies.size();
