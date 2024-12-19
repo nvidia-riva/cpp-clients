594c94c4a3b39f2cfc704409ab252d553839a6ae
Anurag Tomer <atomer@nvidia.com>
2024-12-19 11:16:52 +0530

    Adding initial a2f client changes

--- riva/clients/tts/riva_tts_client.cc
+++ riva/clients/tts/riva_tts_client.cc
@@ -211,6 +211,11 @@ main(int argc, char** argv)
     }
     zero_shot_data->set_sample_rate_hz(zero_shot_sample_rate);
     zero_shot_data->set_quality(FLAGS_zero_shot_quality);
+    if (not FLAGS_zero_shot_text_prompt.empty()) {
+      zero_shot_data->set_text_prompt(FLAGS_zero_shot_text_prompt);
+      assert(!FLAGS_zero_shot_transcript_target.empty());
+      zero_shot_data->set_transcript_target(FLAGS_zero_shot_transcript_target);
+    }
   }
 
   // Send text content using Synthesize().
